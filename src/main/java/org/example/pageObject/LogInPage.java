2e01d332d1337e41edbbacac81caf690f6010684
researchsynergyfoundation <diiahr82@gmail.com>
2023-02-03 21:30:50 +0700

    update directory

--- src/main/java/org/example/pageObject/LogInPage.java
+++ src/main/java/org/example/pageObject/LogInPage.java
@@ -1,4 +1,5 @@
 package org.example.pageObject;
 
 public class LogInPage {
+    //Dibuat hanya 1 file java utk find elemen (user,admin,Host) jika elemnnya sama
 }
--- src/test/java/step_definitions/ProfileSteps.java
+++ src/test/java/step_definitions/ProfileSteps.java
@@ -1,4 +1,4 @@
 package step_definitions;
 
-public class LogInSteps {
+public class ProfileSteps {
 }
