29779942247cec2fb0ed599dba88f6cec8257b2a
researchsynergyfoundation <diiahr82@gmail.com>
2023-02-08 23:36:27 +0700

    update Hooks and add chromedriver for mac

--- src/test/java/step_definitions/Hooks.java
+++ src/test/java/step_definitions/Hooks.java
@@ -4,14 +4,17 @@ import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
 
 public class Hooks {
     public static WebDriver webDriver;
 
     @Before
     public void openBrowser(){
-        System.setProperty("webdriver.chrome.driver",
-                System.getProperty("user.dir") + "\\driver\\chromedriver.exe");
+//        System.setProperty("webdriver.chrome.driver",
+//                System.getProperty("user.dir") + "\\driver\\chromedriver.exe");
+
+        System.setProperty(ChromeDriverService.CHROME_DRIVER_EXE_PROPERTY, "driver/chromedriver");
 
         webDriver = new ChromeDriver();
         String appUrl= "https://campyuk.vercel.app/";
